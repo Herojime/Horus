d9cf6e9cd90dc07405aa0a07786863a4bdff709e
Unknown <helio.jimenez.medina@gmail.com>
2018-05-11 09:40:52 -0700

    Fixed Bases
    Arreglada todas las bases del proyecto listo para implementar nuevos documentos

---
+++ Horus/Horus/Entities/Employee.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Horus.Entities
+{
+    public class Employee
+    {
+        //private string mat = "x";
+        //public string nombreCompleto => nombre + " " + Compare();
+        //public string nombreCompletoSucursal => Sucursal.nombre + " - " + nombreCompleto;
+
+        //internal void CreatePhoneNumbers(int count = 2)
+        //{
+        //    for (int i = 0; i < count; i++)
+        //    {
+        //        Telefono.Add(new Telefono());
+        //    }
+        //}
+
+        //internal void CreateEmails(int count = 1)
+        //{
+        //    for (int i = 0; i < count; i++)
+        //    {
+        //        Correo.Add(new Correo());
+        //    }
+        //}
+
+        //internal string Compare(string mat = "x")
+        //{
+        //    var IsaXin1 = string.Equals(apellidoPaterno, "x", StringComparison.OrdinalIgnoreCase);
+        //    var IsaXin2 = string.Equals(apellidoMaterno, "x", StringComparison.OrdinalIgnoreCase);
+
+        //    var apellidoP = IsaXin1 ? "" : apellidoPaterno;
+        //    var apellidoM = IsaXin2 ? "" : apellidoMaterno;
+
+
+        //    return apellidoP + " " + apellidoM;
+
+        //}
+    }
+}
---
+++ Horus/Horus/Entities/Enum/UserRolesEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Horus.Entities.Enum
+{
+    public class UserRolesEnum
+    {
+        public string Nombre { get; set; }
+        public static UserRolesEnum root = new UserRolesEnum() { Nombre = "root" };
+        public static UserRolesEnum admin = new UserRolesEnum() { Nombre = "admin" };
+        public static UserRolesEnum user = new UserRolesEnum() { Nombre = "user" };
+    }
+}
